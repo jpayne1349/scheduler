57b9c5793d7c1a4306ae9b0c3af5d5b86a48d9f3
James Payne <james.payne1349@gmail.com>
2020-07-07 15:06:38 -0700

    added a new route for saving dates, still in progress

--- app/profile_blueprint/profile.py
+++ app/profile_blueprint/profile.py
@@ -1,26 +1,38 @@
- 
+import re
 
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, jsonify, request
 
-from flask_login import login_required
+from flask_login import login_required, current_user
 
-#from flask import current_app as app
+from app.models import Selection
 
-
-
+from app import db
 
 profile_bp = Blueprint('profile_bp', __name__)
 
-
-#month_day_count = 31
-
+dateFormat = re.compile('[^%22][\d]+') # regular expression object to parse the data from the client
 
 @profile_bp.route('/user/<username>/')
 @login_required
 def user_profile(username):
 
-
     return render_template('user_profile.html') # not sure how to handle the variables at this point
+
+# need a route to call when saving selections
+@profile_bp.route('/save/', methods=['POST'])
+@login_required
+def save():
+    # we can learn alittle regex to parse the data input
+    data = request.get_data()
+    selections = dateFormat.findall(str(data))
+    # have the date strings here. make the new Selection entries into the db
+    for date in selections:
+        new_sel = Selection(date_string=date, user=current_user)
+        db.session.add(new_sel)
+        print("Date added to db:", date)
+    db.session.commit()
+
+    return jsonify("Test Save")
 
 
 """
