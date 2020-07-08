8f3fa8c963308f8163e84aab3affee0209997e6e
James Payne <james.payne1349@gmail.com>
2020-07-08 13:22:51 -0700

    added the functionality to recall the current users date selections from the database and send them to the template on page load. added the database overwrite functionality so their are no duplicate entries per user.

--- app/profile_blueprint/profile.py
+++ app/profile_blueprint/profile.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, flash, jsonify, request
 
 from flask_login import login_required, current_user
 
-from app.models import Selection
+from app.models import Selection, User
 
 from app import db
 
@@ -15,21 +15,32 @@ dateFormat = re.compile('[^%22][\d]+') # regular expression object to parse the 
 @profile_bp.route('/user/<username>/')
 @login_required
 def user_profile(username):
+    old_selections = Selection.query.filter(Selection.user==current_user).all()
+    # we want to send these old_selections through to the javascript to populate the calendar
+    sel_list = []
+    for each in old_selections:
+        sel_list.append(each.date_string)
+    print(sel_list)
 
-    return render_template('user_profile.html') # not sure how to handle the variables at this point
+    return render_template('user_profile.html', old_selections=sel_list)
 
 # need a route to call when saving selections
 @profile_bp.route('/save/', methods=['POST'])
 @login_required
 def save():
-    # we can learn alittle regex to parse the data input
+    old_selections = Selection.query.filter(Selection.user==current_user).all()
+
     data = request.get_data()
-    selections = dateFormat.findall(str(data))
-    # have the date strings here. make the new Selection entries into the db
-    for date in selections:
+    new_selections = dateFormat.findall(str(data))
+    # we could just delete all the prior selections and then add the new ones in...?
+    for each in old_selections:
+        print("deleting date:",each)
+        db.session.delete(each)
+
+    for date in new_selections:
         new_sel = Selection(date_string=date, user=current_user)
         db.session.add(new_sel)
-        print("Date added to db:", date)
+        print("Date added:", date)
     db.session.commit()
 
     return jsonify("Test Save")
